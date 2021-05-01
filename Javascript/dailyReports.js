36d37ffb41bb80e1ea2b2c39405957c55a5648ee
clarelimo <clare.limo@student.moringaschool.com>
2021-05-01 12:16:14 +0300

    display details script added

--- Javascript/dailyReports.js
+++ Javascript/dailyReports.js
@@ -8,5 +8,25 @@ function totalProduction(){
     document.querySelector("#result").innerHTML = sum;
     // alert("The total production is "+sum+ " litres per day");
 }
+var row = 1;
 
+function displayDetails(){
+    var shade = document.getElementById("shade").value;
+    var litres = document.getElementById("litres").value;
+
+    if(!shade || !litres){
+        alert("Please fill all the boxes");
+        return;
+    }
+
+    var display = document.getElementById("display");
+    var newRow = display.insertRow(row);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = shade;
+    cell2.innerHTML = litres;
+    row++;
+    totalProduction();
+}
 
